741e15968f8432d7ab1b231ae4b8a6d0198194b4
NiXc0d3s <bobsokonquestions@gmail.com>
2025-08-05 22:15:34 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const name = "Abober";
+const isAdmin = true;
+
+function Welcome() {
+    console.log("Welcome to ChtoOS!");
+    console.log("v1.0");
+}
+
+function Calculate(a, b) {
+    return a + b;
+}
+
+function Yaroslav22k() {
+    return "Easter egg"
+}
+
+function Vektor(x, y, z) {
+    return "idk lol"
+}
